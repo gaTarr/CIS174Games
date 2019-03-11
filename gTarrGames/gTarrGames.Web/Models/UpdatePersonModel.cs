e348b88fbb0396648879d7d291cd34f4ad984343
Greg Tarr <gregory.a.tarr@gmail.com>
2019-03-11 02:34:21 -0500

    Form Data Validation
    -form validation using simple HTML validation.

--- gTarrGames/gTarrGames.Web/Models/UpdatePersonModel.cs
+++ gTarrGames/gTarrGames.Web/Models/UpdatePersonModel.cs
@@ -1,14 +1,27 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace gTarrGames.Web.Models
 {
     public class UpdatePersonModel
     {
         public Guid PersonId { get; set; }
+
+        [Required]
+        [StringLength(30)]
         public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(30)]
         public string LastName { get; set; }
+
         public string Gender { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Phone]
         public string PhoneNumber { get; set; }
 
     }
