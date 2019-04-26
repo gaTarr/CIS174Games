f7fb72adc3a77d28c490ce0fefa124cb2544b011
Greg Tarr <gregory.a.tarr@gmail.com>
2019-04-26 15:30:24 -0500

    Assignment 7.2 Add an Error Page
    -Implemented error handling via error page.
    -Created generate error page to throw System.OutOfMemoryException
    -For some reason, now profile page is broken only when deployed, still works locally

---
+++ gTarrGames/gTarrGames.Shared/Orchestrators/ErrorLogOrchestrator.cs
@@ -0,0 +1,49 @@
+﻿using gTarrGames.Domain;
+using gTarrGames.Domain.Entities;
+using gTarrGames.Shared.Orchestrators.Interfaces;
+using gTarrGames.Shared.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gTarrGames.Shared.Orchestrators
+{
+    public class ErrorLogOrchestrator : IErrorLogOrchestrator
+    {
+        private readonly GamesContext _gamesContext;
+
+        public ErrorLogOrchestrator()
+        {
+            _gamesContext = new GamesContext();
+        }
+
+        public void CreateError(ErrorLogViewModel errorLogViewModel)
+        {
+            _gamesContext.ErrorLogs.Add(new ErrorLog
+            {
+                ErrorId = errorLogViewModel.ErrorId,
+                ErrorDateTime = errorLogViewModel.ErrorDateTime,
+                ErrorMessage = errorLogViewModel.ErrorMessage,
+                StackTrace = errorLogViewModel.StackTrace,
+                InnerExceptions = errorLogViewModel.InnerExceptions
+            });
+            _gamesContext.SaveChanges();
+        }
+
+        public List<ErrorLogViewModel> GetAllErrors()
+        {
+            var errors = _gamesContext.ErrorLogs.Select(x => new ErrorLogViewModel
+            {
+                ErrorId = x.ErrorId,
+                ErrorDateTime = x.ErrorDateTime,
+                ErrorMessage = x.ErrorMessage,
+                StackTrace = x.StackTrace,
+                InnerExceptions = x.InnerExceptions
+            }).OrderByDescending(x => x.ErrorDateTime).ToList();
+
+            return errors;
+        }
+
+        
+    }
+}
---
+++ gTarrGames/gTarrGames.Web/Api/ErrorLogApiController.cs
@@ -0,0 +1,22 @@
+﻿using gTarrGames.Domain;
+using gTarrGames.Shared.Orchestrators;
+using gTarrGames.Shared.ViewModels;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace gTarrGames.Web.Api
+{
+    public class ErrorLogApiController : ApiController
+    {
+        private readonly ErrorLogOrchestrator _errorLogOrchestrator = new ErrorLogOrchestrator();
+
+
+        [Route("api/v1/errors")]
+        public List<ErrorLogViewModel> GetAllErrors()
+        {
+            var errors = _errorLogOrchestrator.GetAllErrors();
+
+            return errors;
+        }
+    }
+}
